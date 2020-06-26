1d4092e40378172dde1432d3d0a5315e30798fba
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-26 20:42:31 +0530

    Remove Items Using splice()

---
+++ splice.js
@@ -0,0 +1,17 @@
+let array = ['today', 'was', 'not', 'so', 'great'];
+
+array.splice(2, 2);
+// remove 2 elements beginning with the 3rd element
+// array now equals ['today', 'was', 'great']
+
+
+
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+
+let newArray = array.splice(3, 2);
+// newArray equals ['really', 'happy']
+
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(1,4);
+console.log(arr);
