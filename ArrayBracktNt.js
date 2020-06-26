b17698810abc99b5ee70cd3c38ac6272a85b4aee
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-26 20:21:22 +0530

    Access an Array's Contents Using Bracket Notation

---
+++ ArrayBracktNt.js
@@ -0,0 +1,15 @@
+let ourArray = ["a", "b", "c"];
+
+
+let ourVariable = ourArray[0];
+// ourVariable equals "a"
+
+
+
+ourArray[1] = "not b anymore";
+// ourArray now equals ["a", "not b anymore", "c"];
+
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1]="not b";
+console.log(myArray);
