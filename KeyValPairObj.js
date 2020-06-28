986d19dded10d171c6006d60607cd9ecbe485a38
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-28 23:11:25 +0530

    Add Key-Value Pairs to JavaScript Objects

---
+++ KeyValPairObj.js
@@ -0,0 +1,38 @@
+const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true
+};
+
+tekkenCharacter.origin = 'South Korea';
+
+tekkenCharacter['hair color'] = 'dyed orange';
+
+const eyes = 'eye color';
+
+tekkenCharacter[eyes] = 'brown';
+
+
+const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true,
+  origin: 'South Korea',
+  'hair color': 'dyed orange',
+  'eye color': 'brown'
+};
+
+
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods.bananas=13;
+const g='grapes';
+foods[g]=35;
+foods.strawberries=27;
+
+console.log(foods);
